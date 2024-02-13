11d26da7dca05f1b116a8422e3db3ca4c0481f3b
Marcus Young <myoung34@my.apsu.edu>
2024-02-13 17:01:45 -0600

    Use nextdns in unifi

--- terraform/unifi/vlan_nas.tf
+++ terraform/unifi/vlan_nas.tf
@@ -1,8 +1,5 @@
 resource "unifi_network" "nas" {
-  dhcp_dns = [
-    "1.1.1.1",
-    "8.8.8.8",
-  ]
+  dhcp_dns                   = local.nextdns_servers
   dhcp_enabled               = true
   dhcp_relay_enabled         = false
   dhcp_start                 = "192.168.3.100"
