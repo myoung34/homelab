dee57c9bc36badf0d5048603609853278bc17581
Marcus Young <myoung34@my.apsu.edu>
2024-02-13 15:35:36 -0600

    Add local pihole for ez DNS

--- terraform/cloudflare/locals.tf
+++ terraform/cloudflare/locals.tf
@@ -1,6 +1,13 @@
 locals {
   home_ip              = "209.30.119.60"
   marcyoung_us_zone_id = "deeb043448b1d4c5392e31c31a191375"
+  consul_nodes = [
+    "192.168.3.2",
+  ]
+  pihole_nodes = [
+    "192.168.1.21",
+    "192.168.1.27",
+  ]
   github_a_records = [
     "185.199.110.153",
     "185.199.108.153",
