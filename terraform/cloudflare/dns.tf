d2ca24fb3dd0df1a7a36e8da68494be23c80ab9b
Marcus Young <myoung34@my.apsu.edu>
2024-02-13 15:35:36 -0600

    Add local pihole for ez DNS

--- terraform/cloudflare/dns.tf
+++ terraform/cloudflare/dns.tf
@@ -14,3 +14,21 @@ resource "cloudflare_record" "fallback_homelab" {
   type    = "A"
   ttl     = "3600"
 }
+
+resource "cloudflare_record" "consul_fallback" {
+  for_each = toset(local.consul_nodes)
+  zone_id = local.marcyoung_us_zone_id
+  name    = "*.consul"
+  value   = each.value
+  type    = "A"
+  ttl     = "3600"
+}
+
+resource "cloudflare_record" "kube_fallback" {
+  for_each = toset(local.pihole_nodes)
+  zone_id = local.marcyoung_us_zone_id
+  name    = "*.kube"
+  value   = each.value
+  type    = "A"
+  ttl     = "3600"
+}
