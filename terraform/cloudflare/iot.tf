2eb3e8c1a0e253e966a48b32e8e88828ef606f82
Marcus Young <myoung34@my.apsu.edu>
2024-03-15 11:53:07 -0500

    Add starting place for unicorn

--- terraform/cloudflare/iot.tf
+++ terraform/cloudflare/iot.tf
@@ -109,3 +109,11 @@ resource "cloudflare_record" "ble-proxy" {
   type    = "A"
   ttl     = "3600"
 }
+
+resource "cloudflare_record" "unicorn" {
+  zone_id = local.marcyoung_us_zone_id
+  name    = "unicorn.iot.marcyoung.us"
+  value   = "192.168.4.166"
+  type    = "A"
+  ttl     = "3600"
+}
