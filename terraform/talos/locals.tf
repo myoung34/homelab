330d5a0deb330f5306c6387af0b05c712eb4c664
Marcus Young <myoung34@my.apsu.edu>
2024-07-10 16:59:05 +0000

    Upgrade talos to 1.7.5, k8s to 1.30.1

--- terraform/talos/locals.tf
+++ terraform/talos/locals.tf
@@ -1,8 +1,8 @@
 locals {
   cluster_name       = "prod"
   cluster_endpoint   = "https://192.168.1.254:6443"
-  talos_version      = "v1.6.7"
-  kubernetes_version = "v1.29.0"
+  talos_version      = "v1.7.5"
+  kubernetes_version = "v1.30.1"
 
   node_data = {
     controlplanes = {
