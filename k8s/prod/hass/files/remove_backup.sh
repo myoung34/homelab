329f63a732a91ebdf95784b15894d098842e439e
Mark Young <myoung34@my.apsu.edu>
2025-05-05 08:43:34 -0500

    Hass: try to fix remove_backup script

--- k8s/prod/hass/files/remove_backup.sh
+++ k8s/prod/hass/files/remove_backup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 sleep 30
-rm $1
+if [[ -f "$1" ]]; then
+  rm "$1"
+fi
 
 SLACK_WEBHOOK_URL=$(cat /config/secrets.yaml | grep slack_webhook_url | awk '{print $2}' | sed "s/'//g")
+dig hooks.zapier.com >/dev/null 2>&1 # sometimes this somehow fails unless you try to resolve it once before curl
 curl -XPOST "${SLACK_WEBHOOK_URL}" -d "{\"message\": \"Hass backup to minio complete.\"}"
