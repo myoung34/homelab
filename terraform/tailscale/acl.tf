ada7cbe99fbc1b2f31d23277fc393b9d185eef9b
Mark Young <myoung34@my.apsu.edu>
2025-06-11 11:32:00 -0500

    terraform/tailscale: add support for new members to only have mullvad

--- terraform/tailscale/acl.tf
+++ terraform/tailscale/acl.tf
@@ -1,67 +1,76 @@
 resource "tailscale_acl" "acl" {
   acl = <<EOF
-  {
-    // Declare static groups of users. Use autogroups for all users or users with a specific role.
-    // "groups": {
-    //      "group:example": ["alice@example.com", "bob@example.com"],
-    // },
+{
+	// Declare static groups of users. Use autogroups for all users or users with a specific role.
+	// "groups": {
+	//      "group:example": ["alice@example.com", "bob@example.com"],
+	// },
 
-    // Define the tags which can be applied to devices and by which users.
-    "tagOwners": {
-            "tag:k8s-operator": [],
-            "tag:k8s":          ["tag:k8s-operator"],
-            "tag:foo":          [],
-    },
+	// Define the tags which can be applied to devices and by which users.
+	"tagOwners": {
+		"tag:admin-device":  [],
+		"tag:member-device": [],
+		"tag:k8s-operator":  [],
+		"tag:k8s":           ["tag:k8s-operator"],
+    "tag:mullvad":       ["tag:admin-device"],
+	},
 
-    // Define access control lists for users, groups, autogroups, tags,
-    // Tailscale IP addresses, and subnet ranges.
-  	"acls": [
-  		//Allow all connections.
-  		//Comment this section out if you want to define specific restrictions.
-  		{"action": "accept", "src": ["*"], "dst": ["*:*"]},
-  	],
+	// Define access control lists for users, groups, autogroups, tags,
+	// Tailscale IP addresses, and subnet ranges.
+	"acls": [
+		{
+			"action": "accept",
+			"src":    ["tag:member-device"],
+			"dst":    ["tag:mullvad:*"],
+		},
+		{
+			"action": "accept",
+			"src":    ["autogroup:admin", "tag:admin-device", "tag:k8s", "tag:k8s-operator"],
+			"dst":    ["*:*"],
+		},
+	],
 
-  	// Define users and devices that can use Tailscale SSH.
-  	"ssh": [
-  		// Allow all users to SSH into their own devices in check mode.
-  		// Comment this section out if you want to define specific restrictions.
-  		{
-  			"action": "check",
-  			"src":    ["autogroup:member"],
-  			"dst":    ["autogroup:self"],
-  			"users":  ["autogroup:nonroot", "root"],
-  		},
-  		{
-  			"action": "accept",
-  			"src":    ["autogroup:member"],
-  			"dst":    ["tag:k8s"],
-  			"users":  ["3vilpenguin@gmail.com", "autogroup:nonroot", "myoung"],
-  		},
-  	],
-  	"nodeAttrs": [
-  		{
-  			"target": ["*"],
-  			"app": {
-  				"tailscale.com/app-connectors": [],
-  			},
-  		},
-  		{
-  			"target": ["tag:k8s"],
-  			"attr":   ["funnel"],
-  		},
-  		{"target": ["100.125.107.125"], "attr": ["mullvad"]},
-  		{"target": ["100.69.116.77"], "attr": ["mullvad"]},
-  		{"target": ["100.119.170.123"], "attr": ["mullvad"]},
-  	],
+	// Define users and devices that can use Tailscale SSH.
+	"ssh": [
+		// Allow all users to SSH into their own devices in check mode.
+		// Comment this section out if you want to define specific restrictions.
+		{
+			"action": "check",
+			"src":    ["autogroup:member"],
+			"dst":    ["autogroup:self"],
+			"users":  ["autogroup:nonroot", "root"],
+		},
+		{
+			"action": "accept",
+			"src":    ["autogroup:member"],
+			"dst":    ["tag:k8s"],
+      "users":  ["tag:admin-device", "autogroup:nonroot", "myoung"],
+		},
+	],
+	"nodeAttrs": [
+		{
+			"target": ["*"],
+			"app": {
+				"tailscale.com/app-connectors": [],
+			},
+		},
+		{
+			"target": ["tag:k8s"],
+			"attr":   ["funnel"],
+		},
+		{"target": ["100.125.107.125"], "attr": ["mullvad"]},
+		{"target": ["100.69.116.77"], "attr": ["mullvad"]},
+		{"target": ["100.119.170.123"], "attr": ["mullvad"]},
+	],
 
-  	// Test access rules every time they're saved.
-  	// "tests": [
-  	//  	{
-  	//  		"src": "alice@example.com",
-  	//  		"accept": ["tag:example"],
-  	//  		"deny": ["100.101.102.103:443"],
-  	//  	},
-  	// ],
-  }
+	// Test access rules every time they're saved.
+	// "tests": [
+	//  	{
+	//  		"src": "alice@example.com",
+	//  		"accept": ["tag:example"],
+	//  		"deny": ["100.101.102.103:443"],
+	//  	},
+	// ],
+}
   EOF
 }
