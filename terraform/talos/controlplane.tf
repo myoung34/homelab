2fcf7ef90879759037633b8f0bff5c01e91680a6
Mark Young <myoung34@my.apsu.edu>
2026-05-25 22:03:17 -0500

    Fix try/plan in talos tf

--- terraform/talos/controlplane.tf
+++ terraform/talos/controlplane.tf
@@ -8,22 +8,27 @@ data "talos_machine_configuration" "controlplane" {
 resource "talos_machine_configuration_apply" "controlplane" {
   client_configuration        = local.client_configuration
   machine_configuration_input = data.talos_machine_configuration.controlplane.machine_configuration
-  for_each                    = local.node_data.controlplanes
-  node                        = each.key
+
+  for_each = local.node_data.controlplanes
+
+  node = each.key
+
   config_patches = [
     templatefile("${path.module}/templates/install-disk-and-hostname.yaml.tmpl", {
       hostname           = each.value.hostname
       install_disk       = each.value.install_disk
       image              = each.value.image
       machine_token      = local.machine_secrets.secrets.machine_token
-      kubernetes_version = length(each.value.kubernetes_version) == 0 ? local.kubernetes_version : each.value.kubernetes_version
+      kubernetes_version = try(each.value.kubernetes_version, "") == "" ? local.kubernetes_version : each.value.kubernetes_version
     }),
+
     templatefile("${path.module}/templates/controlplane.yaml.tmpl", {
-      talos_version         = length(each.value.talos_version) == 0 ? local.talos_version : each.value.talos_version
-      kubernetes_version    = length(each.value.kubernetes_version) == 0 ? local.kubernetes_version : each.value.kubernetes_version
+      talos_version         = try(each.value.talos_version, "") == "" ? local.talos_version : each.value.talos_version
+      kubernetes_version    = try(each.value.kubernetes_version, "") == "" ? local.kubernetes_version : each.value.kubernetes_version
       network_hardware_addr = each.value.network_hardware_addr
       cert_sans             = local.cert_sans
     }),
+
     templatefile("${path.module}/templates/extensionserviceconfig.yaml.tmpl", {
       name = "tailscale"
       env  = local.extensions.tailscale.env
